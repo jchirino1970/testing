b57500ad29cb2bfcf0db126b086d55c6e30aa5f7
jechirino <jechirino@overon.es>
2020-07-04 20:44:16 +0200

    cambios en karma

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+      }
+
+    }, 
+    singleRun: true,
     restartOnFileChange: true
   });
 };
