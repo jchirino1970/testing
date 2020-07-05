fa09191c393c4b2b20cab471935519d5fae6b203
jechirino <jechirino@overon.es>
2020-07-05 17:34:14 +0200

    modificiaciones de karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const { __metadata } = require('tslib')
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,17 +12,36 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-sonarqube-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/testing'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+          reports: ['html', 'lcovonly', 'text-summary'],
+          fixWebpackSourcePaths: true,
+          thresholds: {
+            statements: 100,
+            lines: 100,
+            branches: 100,
+            functions: 100
+          } 
+      },
+      angularCli: {
+        environment: 'dev'
+      },
+    sonarqubeReporter: {
+      basePath: 'src/app',
+      outputFolder: 'reports',
+      filePattern: '**/*spec.ts',
+      encoding: 'utf-8',
+      legacyMode: false,
+      reportName: (metadat)=>{
+        return metadat.concat('xml').join('.');
+      }
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'sonarqube'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
